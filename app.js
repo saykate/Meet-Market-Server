ed170916f724f903ea3538da87f3d332a7624532
Katie Sayers <kate@student.dev-island.org>
2024-04-16 19:12:27 -1000

    removed .render function

--- app.js
+++ app.js
@@ -32,8 +32,9 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({
+    error: err, 
+  });
 });
 
 module.exports = app;
