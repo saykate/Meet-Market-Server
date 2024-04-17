fb3e30b95de102324b3c93511d935a933177e7f1
Katie Sayers <kate@student.dev-island.org>
2024-04-16 19:03:29 -1000

    testing deployment

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 require("dotenv").config();
-const createError = require('http-errors');
-const express = require('express');
-const cors = require("cors")
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const app = express();
 
-app.use(cors())
-app.use(logger('dev'));
+const whitelist = ["http://localhost:5173", "https://meet-market.netlify.app"];
+const corsOptions = { origin: whitelist, credentials: true };
+app.use(cors(corsOptions));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
